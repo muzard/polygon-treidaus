87d5e2d1f34c47a7a27e3ee1e270f88f1f904172
Arttu Kokki <arttu.kokki@gmail.com>
2025-11-15 14:57:47 +0200

    init js version

--- connection.py
+++
@@ -1,9 +0,0 @@
-from py_clob_client.client import ClobClient
-from py_clob_client.clob_types import OrderArgs, OrderType
-from py_clob_client.order_builder.constants import BUY
-import requests as req
-
-price_url = "https://clob.polymarket.com/price"
-price_res = req.url(price_url)
-# returns JSON { "price" : 1234.0}
-
